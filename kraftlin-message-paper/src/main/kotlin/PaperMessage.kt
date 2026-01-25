5a53f752be68f068a0f4b30426cf2cbd9017ca37
minoneer <minoneer@gmail.com>
2026-01-25 18:09:46 +0100

    Message: Feat: add `CommandSender.message(String, ClickableText.() -> Unit)` extension for convenience

--- kraftlin-message-paper/src/main/kotlin/PaperMessage.kt
+++ kraftlin-message-paper/src/main/kotlin/PaperMessage.kt
@@ -5,19 +5,24 @@ import io.github.kraftlin.message.ClickableText
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
 import org.bukkit.command.CommandSender
+import io.github.kraftlin.message.message as buildMessage
 
 
 public fun CommandSender.message(block: ClickableMessage.() -> Unit): Unit =
-    sendMessage(io.github.kraftlin.message.message(block))
+    sendMessage(buildMessage(block))
 
 public fun CommandSender.message(text: String, color: TextColor? = null) {
     if (color == null) {
-        sendMessage(io.github.kraftlin.message.message(text))
+        sendMessage(buildMessage(text))
     } else {
-        sendMessage(io.github.kraftlin.message.message(text, color))
+        sendMessage(buildMessage(text, color))
     }
 }
 
-public fun CommandSender.message(component: Component, init: ClickableText.() -> Unit = {}) {
-    sendMessage(io.github.kraftlin.message.message(component, init))
+public fun CommandSender.message(text: String, block: ClickableText.() -> Unit) {
+    sendMessage(buildMessage(text, block))
+}
+
+public fun CommandSender.message(component: Component, block: ClickableText.() -> Unit = {}) {
+    sendMessage(buildMessage(component, block))
 }
